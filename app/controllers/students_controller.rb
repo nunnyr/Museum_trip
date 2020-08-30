462982430a2fab71261810ef9eed4d0c2dcf598f
Nunny R <nunnyreyes@gmail.com>
2020-08-30 19:27:42 -0400

    updating routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,19 @@
 class StudentsController < ApplicationController
+    
+    def index
+        @students = Student.all
+    end
+
+
+
+
+
+
+
+
+
+
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #student routes
+  get "/students", to: "students#index", as: "students"
+  get "/students/:id", to: "students#show", as: "student"
+
+
+  #museum routes
+  get "/museums", to: "museums#index", as: "museums"
+  get "/museums/:id", to: "museums#show", as: "museum"
+
+
+  #trip routes
+  get "/trips", to: "trips#index", as: "trips"
+  get "/trips/:id", to: "trips#show", as: "trip"
+
 end
